3d5199f76c8d01c77f766834b478e09180bdd9dc
Peter Florek <peter.florek@yahoo.com>
2018-11-29 22:50:55 -0600

    initial setup

---
+++ liri.js
@@ -0,0 +1,9 @@
+const request = require("request");
+const Spotify = require("node-spotify-api");
+const moment = require("moment");
+
+require("dotenv").config();
+
+const spotifyKeys = require("./keys.js");
+
+const spotify = new Spotify(spotifyKeys.spotify);
